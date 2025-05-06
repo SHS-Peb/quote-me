734056446477b6f6d708ae94ada5d0810bcb6b09
unknown <blackskiesdancer@gmail.com>
2025-05-06 19:17:25 +1000

    js code

---
+++ index.js
@@ -0,0 +1,12 @@
+function getQuote() {
+    const script = document.createElement('script');
+    script.src = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=handleQuote';
+    document.body.appendChild(script);
+  }
+
+function handleQuote(response) {
+    document.getElementById('quote').textContent = response.quoteText;
+    document.getElementById('author').textContent = response.quoteAuthor || 'Unknown';
+  }
+  
+  document.getElementById('new-quote').addEventListener('click', getQuote);
